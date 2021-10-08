2003883cb40eea265a07672c25f82ca0b0629b8f
Raline Silva <ralinesilva1@gmail.com>
2021-10-08 14:18:03 -0300

    Criar classe venda

---
+++ src/br/com/zup/Venda.java
@@ -0,0 +1,62 @@
+package br.com.zup;
+
+public class Venda {
+    private Cliente cliente;
+    private Vendedor vendedorResponsavel;
+    private double valorDaVenda;
+    private String dataDeRegistro;
+
+    public Venda() {
+
+    }
+
+    public Venda(Cliente cliente, Vendedor vendedorResponsavel, double valorDaVenda, String dataDeRegistro) {
+        this.cliente = cliente;
+        this.vendedorResponsavel = vendedorResponsavel;
+        this.valorDaVenda = valorDaVenda;
+        this.dataDeRegistro = dataDeRegistro;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vendedor getVendedorResponsavel() {
+        return vendedorResponsavel;
+    }
+
+    public void setVendedorResponsavel(Vendedor vendedorResponsavel) {
+        this.vendedorResponsavel = vendedorResponsavel;
+    }
+
+    public double getValorDaVenda() {
+        return valorDaVenda;
+    }
+
+    public void setValorDaVenda(double valorDaVenda) {
+        this.valorDaVenda = valorDaVenda;
+    }
+
+    public String getDataDeRegistro() {
+        return dataDeRegistro;
+    }
+
+    public void setDataDeRegistro(String dataDeRegistro) {
+        this.dataDeRegistro = dataDeRegistro;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("***Extrato da Venda****");
+        retorno.append("\n Cliente: " + getCliente());
+        retorno.append("\n Vendedor: " + getVendedorResponsavel());
+        retorno.append("\n Valor da Venda: " + valorDaVenda);
+        retorno.append("\n Data de Registro: " + dataDeRegistro);
+        return retorno.toString();
+    }
+}
