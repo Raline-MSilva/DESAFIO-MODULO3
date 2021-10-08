d574e4dcfd4ac850a85b34621523d557b5db5a1f
Raline Silva <ralinesilva1@gmail.com>
2021-10-08 18:37:35 -0300

    Criar menu

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Sistema.cadastrarVendedor();
-            Sistema.cadastrarVendedor();
+            Sistema.Executar();
 
 
         }catch (Exception deuErro){
--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -10,6 +10,19 @@ public class Sistema {
         return capturarDados;
     }
 
+    //Criar menu
+    public static void menu (){
+        System.out.println("**Bem vindo ao sistema de Vendas Você em Divída!**");
+        System.out.println("Digite 1 para cadastrar um cliente");
+        System.out.println("Digite 2 para cadastrar um vendedor");
+        System.out.println("Digite 3 para realizar uma venda");
+        System.out.println("Digite 4 para exibir a lista de Clientes");
+        System.out.println("Digite 5 para exibir a lista de vendedores");
+        System.out.println("Digite 6 para exibir a lista de vendas");
+        System.out.println("Digite 7 para sair do Você em Divída");
+    }
+
+
     //cadastrar cliente
     public static Cliente cadastrarCliente() throws Exception{
         String nome = capturarDados("Por favor, informe o nome do Cliente: ").nextLine();
@@ -43,4 +56,5 @@ public class Sistema {
 
         return ServicoVenda.cadastrarVenda(cliente, vendedor, valorDaVenda, dataDeRegistro);
     }
+
 }
