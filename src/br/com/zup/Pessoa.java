ee1b952b29d53c4ec1ba53ac9642087a36f1a51b
Raline Silva <ralinesilva1@gmail.com>
2021-10-08 11:53:49 -0300

    Criar classe pessoa

---
+++ src/br/com/zup/Pessoa.java
@@ -0,0 +1,50 @@
+package br.com.zup;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String email;
+
+    public Pessoa() {
+
+    }
+
+    public Pessoa(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\n Nome: " + nome);
+        retorno.append("\n CPF: " + cpf);
+        retorno.append("\n Email: " + email);
+        return retorno.toString();
+    }
+}
